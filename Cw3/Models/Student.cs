9cc03735a8f5037b4ee97d9b1aa39679afef1738
mikus590 <s17371@pjwstk.edu.pl>
2020-03-28 22:04:25 +0100

    repair a git clone

---
+++ Cw3/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cw3.Models
+{
+    public class Student
+    {
+        // GET: /<controller>/
+        public int IdStudent
+        {
+            get; set;
+        }
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        public string IndexNumber
+        {
+            get; set;
+        }
+    }
+}
